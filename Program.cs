f6c37d6cc72697a20fd4c246c51f6b1ea68bd13d
Bruno Mota <bmotadev@gmail.com>
2022-11-08 15:26:42 -0300

    implementando o metodo listar alunos

--- Program.cs
+++ Program.cs
@@ -3,7 +3,55 @@
 Pessoa p1 = new Pessoa();
 p1.Nome = "Bruno";
 p1.Sobrenome = "Mota";
-p1.Idade = 31;
-//p1._nome = "";
 
-p1.Apresentar();
+Pessoa p2 = new Pessoa();
+p2.Nome = "Bruno";
+p2.Sobrenome = "Oliveira";
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Bruno";
+// p1.Sobrenome = "Mota";
+// p1.Idade = 31;
+// //p1._nome = "";
+
+// p1.Apresentar();
